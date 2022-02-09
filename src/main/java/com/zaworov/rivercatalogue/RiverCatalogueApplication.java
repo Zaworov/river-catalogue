de1484409041a4758fa9cbf972289557d5f0a502
zawodpi <zawodpi@hlag.com>
2022-02-09 15:55:33 +0100

    KAJ-5 Bypass test error while bulding (DB not ready yet)

--- src/main/java/com/zaworov/rivercatalogue/RiverCatalogueApplication.java
+++ src/main/java/com/zaworov/rivercatalogue/RiverCatalogueApplication.java
@@ -2,8 +2,9 @@ package com.zaworov.rivercatalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) //TODO Remove this (KAJ-13)
 public class RiverCatalogueApplication {
 
 	public static void main(String[] args) {

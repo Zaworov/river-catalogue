2da8388ed5aa5907f1bbb6b06d352e8be92c1b8a
zawodpi <zawodpi@hlag.com>
2022-02-09 15:50:14 +0100

    KAJ-5 Initial commit

---
+++ src/main/java/com/zaworov/rivercatalogue/RiverCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.zaworov.rivercatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiverCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RiverCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaworov/rivercatalogue/RiverCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaworov.rivercatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiverCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

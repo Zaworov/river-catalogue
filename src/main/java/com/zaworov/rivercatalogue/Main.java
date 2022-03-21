57b91364b5c1fa5ad0ae8c91440500e64385ad9c
zawodpi <zawodpi@hlag.com>
2022-03-21 22:43:25 +0100

    KAJ-5 Added controller method to main class + test

--- src/main/java/com/zaworov/rivercatalogue/Main.java
+++ src/main/java/com/zaworov/rivercatalogue/Main.java
@@ -3,12 +3,20 @@ package com.zaworov.rivercatalogue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class }) //TODO Remove this (KAJ-13)
-public class RiverCatalogueApplication {
+public class Main {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(RiverCatalogueApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
